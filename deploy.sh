d6155f8c79fefe9b02dff31a1c054cfdc31e00ad
DarlingOlvera <dmop0600@gmail.com>
2023-02-21 17:10:14 -0600

    token3

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,5 @@ sudo git pull
 #Encender nginx otra vez
 sudo systemctl start nginx
 
-#configurar authToken
-sudo ngrok config add-authtoken 2M4Iq6mi2bBdbESybikwU489rbN_Rnm5NtZhTiBmAaJMpTMF
 #Encender ngrok
 sudo ngrok http 80
